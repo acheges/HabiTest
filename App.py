c7b7d03aa6d5417fbd4cc76c04a51b429b032594
Humberto <humberto@diogenes.com>
2021-09-26 00:51:34 -0500

    02.Testing_routes

--- App.py
+++ App.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,jsonify
+import json
 from flask_mysqldb import MySQL
 
 # Initiate the connection
@@ -25,38 +26,47 @@ def index():
 def addcontact():
     return 'Aca aparecerán todos'
 
-# @app.route('/properties')
-# def users():
-#     cur = mysql.connection.cursor()
-#     cur.execute('''SELECT user, host FROM mysql.user''')
-#     rv = cur.fetchall()
-#     return str(rv)
-
 @app.route('/properties')
 def showproperties():
     cur = mysql.connection.cursor()
     cur.execute('SELECT * FROM property')
+    # Getting the headers
+    row_headers=[x[0] for x in cur.description]
     data = cur.fetchall()
-    cur.close()
-    return str(data)
-    #return render_template('index.html', contacts = data)
+    json_data = []
+    for result in data:
+        json_data.append(dict(zip(row_headers,result)))
+    
+    return jsonify(json_data)
+    
+
 
 @app.route('/status')
 def showstatus():
     cur = mysql.connection.cursor()
     cur.execute('SELECT * FROM status')
+    # Getting the headers
+    row_headers=[x[0] for x in cur.description]
     data = cur.fetchall()
+    json_data = []
+    for result in data:
+        json_data.append(dict(zip(row_headers,result)))
     cur.close()
-    return str(data)
-    #return render_template('index.html', contacts = data)
+    return jsonify(json_data)
+
 
 @app.route('/status_history')
 def showstatushistory():
     cur = mysql.connection.cursor()
     cur.execute('SELECT * FROM status_history')
+    # Getting the headers
+    row_headers=[x[0] for x in cur.description]
     data = cur.fetchall()
+    json_data = []
+    for result in data:
+        json_data.append(dict(zip(row_headers,result)))
     cur.close()
-    return str(data)
+    return jsonify(json_data)
 
 @app.route('/fulljoin')
 def showfulljoin():
@@ -65,13 +75,34 @@ def showfulljoin():
         '''SELECT * 
         FROM status_history
         INNER JOIN status
-        ON status_history.status_id = status.id ''')
+        ON status_history.status_id = status.id
+        INNER JOIN property
+        ON property.id = status_history.property_id ''')
+    # Getting the headers
+    row_headers=[x[0] for x in cur.description]
     data = cur.fetchall()
+    json_data = []
+    for result in data:
+        json_data.append(dict(zip(row_headers,result)))
     cur.close()
-    return str(data)
+    return jsonify(json_data)
 
+@app.route('/last_status')
+def last_status():
+    cur = mysql.connection.cursor()
+    cur.execute(
+        '''SELECT property_id, MAX(update_date)
+        FROM status_history
+        GROUP BY property_id ''')
 
-
+    # Getting the headers
+    row_headers=[x[0] for x in cur.description]
+    data = cur.fetchall()
+    json_data = []
+    for result in data:
+        json_data.append(dict(zip(row_headers,result)))
+    cur.close()
+    return jsonify(json_data)
 
 if __name__ == '__main__':
     app.run(port= 3000, debug= True)
