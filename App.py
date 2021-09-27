23716e8f23d56047efdac7ab7485de2bebf11178
Humberto <humberto@diogenes.com>
2021-09-27 00:50:35 -0500

    04.Small_changes_filter_test

--- App.py
+++ App.py
@@ -1,5 +1,6 @@
-from flask import Flask, request, jsonify
 import json
+
+from flask import Flask, jsonify, request
 from flask_mysqldb import MySQL
 
 # Initiate the connection
@@ -13,15 +14,16 @@ app.config["MYSQL_PASSWORD"] = "VGbt3Day5R"
 app.config["MYSQL_DB"] = "habi_db"
 mysql = MySQL(app)
 
-# settings for memory app
+# Settings for memory app
 app.secret_key = "example"
 
 
 @app.route("/")
 def index():
-    return "Hello Humberto"
+    """ Basic definition of the project """
+    return """Api Test for Habi"""
 
-
+### Exploring the tables
 @app.route("/properties")
 def showproperties():
     """Showing all the properties"""
@@ -67,63 +69,25 @@ def showstatushistory():
     return jsonify(json_data)
 
 
-@app.route("/fulljoin")
-def showfulljoin():
-    """Testing a Full Inner Join"""
-    cur = mysql.connection.cursor()
-    cur.execute(
-        """SELECT * 
-        FROM status_history
-        INNER JOIN status
-        ON status_history.status_id = status.id
-        INNER JOIN property
-        ON property.id = status_history.property_id """
-    )
-    # Getting the headers
-    row_headers = [x[0] for x in cur.description]
-    data = cur.fetchall()
-    json_data = []
-    for result in data:
-        json_data.append(dict(zip(row_headers, result)))
-    cur.close()
-    return jsonify(json_data)
-
-
-@app.route("/last_status")
-def last_status():
-    """Displaying only the Last Status from the Status_History"""
-    cur = mysql.connection.cursor()
-    cur.execute(
-        """
-        SELECT
-        last_status_table.property_id, 
-        status_history.id, 
-        last_status_table.last_update
-        FROM status_history 
-        INNER JOIN
-        (SELECT property_id, MAX(update_date) AS last_update
-        FROM status_history
-        GROUP BY property_id) AS last_status_table
-        ON last_status_table.property_id = status_history.property_id
-        AND last_status_table.last_update = status_history.update_date
-        """
-    )
-    # Getting the headers
-    row_headers = [x[0] for x in cur.description]
-    data = cur.fetchall()
-    json_data = []
-    for result in data:
-        json_data.append(dict(zip(row_headers, result)))
-    cur.close()
-    return jsonify(json_data)
-
-
-@app.route("/api/v1/updatedproperties", methods=['GET'])
+@app.route("/api/v1/updatedproperties", methods=["GET"])
 def last_status_fulljoin():
     """Displaying only the Last Status from the Status_History with more information"""
     cur = mysql.connection.cursor()
+    # We try to avoid unnecessary redundancy
     query = """
-        SELECT * FROM 
+        SELECT
+        property.address,
+        property.city,
+        property.price,
+        property.description,
+        property.year,
+        stat.property_id,
+        stat.status_id,
+        stat.last_update,
+        status.name,
+        status.label
+        
+        FROM 
         property 
         INNER JOIN
 
@@ -140,39 +104,37 @@ def last_status_fulljoin():
         GROUP BY property_id) AS last_status_table
         ON last_status_table.property_id = status_history.property_id
         AND last_status_table.last_update = status_history.update_date
-        ) 
+        ) AS stat
 
-        AS stat 
         ON property.id = stat.property_id
         INNER JOIN
         status
         ON status.id = stat.status_id
         """
     query_parameters = request.args
-    #------------------------------------------------
-    status_id = query_parameters.get('status_id')
-    property_id = query_parameters.get('property_id')
-    city = query_parameters.get('city')
-    #------------------------------------------------
+    # ------------------------------------------------
+    status_id = query_parameters.get("status_id")
+    property_id = query_parameters.get("property_id")
+    city = query_parameters.get("city")
+    # ------------------------------------------------
+    # We add optional filters
+    #-------------------------------------------------
     filters = []
     if status_id:
-        #query += ' id=? AND'
-        filters.append(status_id)
+        
+        filters.append(f"status_id = {status_id}")
     if property_id:
-        #query += ' published=? AND'
-        filters.append(property_id)
+        filters.append(f"property_id = {property_id}")
     if city:
-        #query += ' author=? AND'
-        filters.append(city)
+        filters.append(f"city = {city}")
 
-    print(filters)
-
-
-    
-    print(id)
-    # We get the filters
-    
-    cur.execute(query)
+    if filters:
+        add_filters = (f""" WHERE {" AND ".join(filters)}""")
+        print(add_filters)
+        cur.execute(query + add_filters)
+    else:
+        cur.execute(query)
+   
     # Getting the headers
     row_headers = [x[0] for x in cur.description]
     data = cur.fetchall()
@@ -181,7 +143,6 @@ def last_status_fulljoin():
         json_data.append(dict(zip(row_headers, result)))
     cur.close()
     return jsonify(json_data)
-
 
 if __name__ == "__main__":
     app.run(port=3000, debug=True)
